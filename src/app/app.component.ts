dc8d27981368f4c73f41a53d86b3da5d961d9616
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-11-12 18:24:12 +0100

    change app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { appName } from './app.config';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Frontend';
+  readonly title = appName;
 }
