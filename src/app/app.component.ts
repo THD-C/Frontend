64207ae820cbd607bec2b95cac236f968d70eacf
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-11-13 22:35:26 +0100

    add devextreme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { DxButtonModule } from 'devextreme-angular/ui/button';
+
 import { appName } from './app.config';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DxButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
