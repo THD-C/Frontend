e2bc32803d0da0e4b62893b19d33115e5c882a41
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-11-17 22:50:38 +0100

    improve form validation

--- src/app/modules/register/components/register/register.component.ts
+++ src/app/modules/register/components/register/register.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ValidationCallbackData } from 'devextreme/common';
-import { ClickEvent } from 'devextreme/ui/button';
 import { TextBoxType } from 'devextreme/ui/text_box';
+import { DxTextBoxComponent } from 'devextreme-angular/ui/text-box';
 
 import { RegisterRequest } from './register.model';
 import { passwordButtonOptions } from './register.config';
@@ -16,7 +16,12 @@ import { AuthService } from '../../../../services/user/auth.service';
 })
 export class RegisterComponent {
 
-  constructor(private readonly authService: AuthService) {}
+  readonly passwordButtonOptions = {
+    ...passwordButtonOptions,
+    onClick: () => {
+      this.passwordMode = this.passwordMode === 'text' ? 'password' : 'text';
+    },
+  };
 
   passwordMode: TextBoxType = 'password';
   newUser: RegisterRequest = {
@@ -32,19 +37,26 @@ export class RegisterComponent {
     return this.passwordErrors.length > 0;
   }
 
-  readonly passwordButtonOptions = {
-    ...passwordButtonOptions,
-    onClick: () => {
-      this.passwordMode = this.passwordMode === 'text' ? 'password' : 'text';
-    },
-  };
+  get isFormValid(): boolean {
+    return this.newUser.username.length > 0
+      && this.txtEmail.isValid
+      && validatePassword(this.newUser.password).length === 0;
+  }
+
+  @ViewChild('txtEmail') txtEmail!: DxTextBoxComponent;
+
+  constructor(private readonly authService: AuthService) {}
 
   validatePassword(callbackData: ValidationCallbackData): boolean {
     this.passwordErrors = validatePassword(callbackData.value);
     return this.passwordErrors.length === 0;
   }
 
-  async submit(clickEvent: ClickEvent): Promise<void> {
+  async submit(): Promise<void> {
+    if (this.isFormValid === false) {
+      return;
+    }
+
     try {
       await this.authService.register(this.newUser);
     } catch(e) {
