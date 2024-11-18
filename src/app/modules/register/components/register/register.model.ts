2ed0706ebf20af06574911cbe2295159f0c425d6
Peter <62666278+GringoXY@users.noreply.github.com>
2024-11-18 22:54:24 +0100

    Fix sign up view (#18)
    * fix registration
    
    * remove console.log
    
    * expand registration process
    
    * improve responsiveness
    
    * add step info
    
    * fix routing

--- src/app/modules/register/components/register/register.model.ts
+++ src/app/modules/register/components/register/register.model.ts
@@ -4,5 +4,28 @@ export type RegisterRequest = {
   username: string;
   email: string;
   password: string;
+  name: string;
+  surname: string;
+  street: string;
+  building: string;
+  city: string;
+  postal_code: string;
+  country: string;
 }
 export type RegisterResponse = Session;
+
+export enum RegisterStep {
+  Credentials,
+  PersonalData,
+  Address
+}
+
+export const nextStepsMap: Map<RegisterStep, RegisterStep> = new Map([
+  [RegisterStep.Credentials, RegisterStep.PersonalData],
+  [RegisterStep.PersonalData, RegisterStep.Address],
+]);
+
+export const previousStepsMap: Map<RegisterStep, RegisterStep> = new Map([
+  [RegisterStep.Address, RegisterStep.PersonalData],
+  [RegisterStep.PersonalData, RegisterStep.Credentials],
+]);
--- src/app/modules/register/register.routes.ts
+++ src/app/modules/register/register.routes.ts
@@ -1,9 +1,26 @@
 import { Routes } from '@angular/router';
+
 import { RegisterComponent } from './components/register/register.component';
+import { RegisterCredentialsComponent } from './components/register/register-credentials/register-credentials.component';
+import { RegisterAddressComponent } from './components/register/register-address/register-address.component';
 
 export const routes: Routes = [
   {
-    path: '**',
+    path: '',
     component: RegisterComponent,
+    children: [
+      {
+        path: 'credentials',
+        component: RegisterCredentialsComponent,
+      },
+      {
+        path: 'personal-data',
+        component: RegisterCredentialsComponent,
+      },
+      {
+        path: 'address',
+        component: RegisterAddressComponent,
+      },
+    ]
   },
 ];
