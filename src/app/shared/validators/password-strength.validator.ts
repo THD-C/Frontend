889e3cdb3ee11a5c728a8373170438ee8e9fca81
Peter <62666278+GringoXY@users.noreply.github.com>
2024-11-21 00:42:58 +0100

    Add i18n (#22)
    * add localize package and improvie scripts
    
    * prepare translation script
    
    * update README.md about translations
    
    * add login translations
    
    * add register translations
    
    * add register's subcomponents translations

--- src/app/shared/validators/password-strength.validator.ts
+++ src/app/shared/validators/password-strength.validator.ts
@@ -1,17 +1,17 @@
 export const lowerCaseRegex: RegExp = /[a-z]+/;
-export const lowerCaseNoMatchErrorMessage: string = 'Password must contain at least 1 lower case character';
+export const lowerCaseNoMatchErrorMessage: string = $localize`:@@password-strength-validator.Password-must-contain-at-least-1-lower-case-character:Password must contain at least 1 lower case character`;
 
 export const upperCaseRegex: RegExp = /[A-Z]+/;
-export const upperCaseNoMatchErrorMessage: string = 'Password must contain at least 1 upper case character';
+export const upperCaseNoMatchErrorMessage: string = $localize`:@@password-strength-validator.Password-must-contain-at-least-1-upper-case-character:Password must contain at least 1 upper case character`;
 
 export const digitRegex: RegExp = /\d+/;
-export const digitNoMatchErrorMessage: string = 'Password must contain at least 1 digit';
+export const digitNoMatchErrorMessage: string = $localize`:@@password-strength-validator.Password-must-contain-at-least-1-digit:Password must contain at least 1 digit`;
 
 export const specialCharRegex: RegExp = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/;
-export const specialCharNoMatchErrorMessage: string = 'Password must contain at least 1 special character';
+export const specialCharNoMatchErrorMessage: string = $localize`:@@password-strength-validator.Password-must-contain-at-least-1-special-character:Password must contain at least 1 special character`;
 
 export const minLength: number = 8;
-export const minLengthErrorMessage: string = 'Password must be at least 8 characters long';
+export const minLengthErrorMessage: string = $localize`:@@password-strength-validator.Password-must-be-at-least-8-characters-long:Password must be at least 8 characters long`;
 
 export const validatePassword = function (password: string): string[] {
   const validationResult: [boolean, string][] = [
