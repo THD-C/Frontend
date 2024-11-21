cbccc6f91239d96124e861f361ea1de68c6942b2
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-11-21 23:25:48 +0100

    add main home section

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { appName } from '../../../../app.config';
+import { greenCandleColor, redCandleColor, StockPrice, stockPrices } from './home.model';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,18 @@ import { appName } from '../../../../app.config';
 })
 export class HomeComponent {
 
+  protected readonly greenCandleColor = greenCandleColor;
+  protected readonly redCandleColor = redCandleColor;
   protected readonly appName = appName;
+  stockPrices: StockPrice[] = stockPrices;
+
+  customizeTooltip(arg: any) {
+    return {
+      text: `Open: $${arg.openValue}<br/>`
+                + `Close: $${arg.closeValue}<br/>`
+                + `High: $${arg.highValue}<br/>`
+                + `Low: $${arg.lowValue}<br/>`,
+    };
+  }
   
 }
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DxChartModule } from 'devextreme-angular/ui/chart';
 import { provideRouter, RouterLink, RouterModule } from '@angular/router';
 import { routes } from './home.routes';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { DxButtonModule } from 'devextreme-angular/ui/button';
 
 
 @NgModule({
@@ -19,6 +21,8 @@ import { HeaderComponent } from '../../shared/components/header/header.component
     ReactiveFormsModule,
     RouterModule,
     RouterLink,
+    DxChartModule,
+    DxButtonModule,
 
     HeaderComponent,
   ]
