5299ae35b09515e0f7947fafa5e2801d8a92bf1e
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-11-22 22:28:46 +0100

    add translations for home page

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { appName } from '../../../../app.config';
 import { CryptoInfo as CryptoInfo, greenCandleColor, redCandleColor, StockPrice, stockPrices, cryptosInfo, cryptoStockPrices, CryptoStockPrice, UserGrowth, usersGrowth } from './home.model';
+import { newsletterButtonOptions } from './home.config';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +13,7 @@ export class HomeComponent {
   protected readonly greenCandleColor = greenCandleColor;
   protected readonly redCandleColor = redCandleColor;
   protected readonly appName = appName;
+  protected readonly newsletterButtonOptions = newsletterButtonOptions;
   stockPrices: StockPrice[] = stockPrices;
 
   cryptosInfo: CryptoInfo[] = cryptosInfo;
---
+++ src/app/modules/home/components/home/home.config.ts
@@ -0,0 +1,6 @@
+import { DxButtonTypes } from 'devextreme-angular/ui/button';
+
+export const newsletterButtonOptions: DxButtonTypes.Properties = {
+  useSubmitBehavior: false,
+  text: $localize`:@@home.Submit:Submit`,
+}
