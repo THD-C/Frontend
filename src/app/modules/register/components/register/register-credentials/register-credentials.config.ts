9fdc4c6201032cd671bf167c0ba5ede7aed69a73
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-11-24 20:11:28 +0100

    remove unused components

--- src/app/modules/register/components/register/register-credentials/register-credentials.config.ts
+++
@@ -1,7 +0,0 @@
-import { DxButtonTypes } from 'devextreme-angular/ui/button';
-
-export const passwordButtonOptions: DxButtonTypes.Properties = {
-  icon: 'eyeopen',
-  stylingMode: 'text',
-  useSubmitBehavior: false,
-}
