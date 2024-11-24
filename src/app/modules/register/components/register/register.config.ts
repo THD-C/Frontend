bd2460c61b44bd35737c033b60a46143d4cacc05
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-11-24 20:10:04 +0100

    simplify registration

---
+++ src/app/modules/register/components/register/register.config.ts
@@ -0,0 +1,7 @@
+import { DxButtonTypes } from 'devextreme-angular/ui/button';
+
+export const passwordButtonOptions: DxButtonTypes.Properties = {
+  icon: 'eyeopen',
+  stylingMode: 'text',
+  useSubmitBehavior: false,
+}
