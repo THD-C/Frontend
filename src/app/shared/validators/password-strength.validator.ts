9fb316763e288c9c791dbf83d7897d1f189ab0ad
Smithski <62666278+GringoXY@users.noreply.github.com>
2024-12-14 11:15:32 +0100

    change password validation on registration (#47)

--- src/app/shared/validators/password-strength.validator.ts
+++ src/app/shared/validators/password-strength.validator.ts
@@ -1,24 +1,8 @@
-export const lowerCaseRegex: RegExp = /[a-z]+/;
-export const lowerCaseNoMatchErrorMessage: string = $localize`:@@password-strength-validator.Password-must-contain-at-least-1-lower-case-character:Password must contain at least 1 lower case character`;
-
-export const upperCaseRegex: RegExp = /[A-Z]+/;
-export const upperCaseNoMatchErrorMessage: string = $localize`:@@password-strength-validator.Password-must-contain-at-least-1-upper-case-character:Password must contain at least 1 upper case character`;
-
-export const digitRegex: RegExp = /\d+/;
-export const digitNoMatchErrorMessage: string = $localize`:@@password-strength-validator.Password-must-contain-at-least-1-digit:Password must contain at least 1 digit`;
-
-export const specialCharRegex: RegExp = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]+/;
-export const specialCharNoMatchErrorMessage: string = $localize`:@@password-strength-validator.Password-must-contain-at-least-1-special-character:Password must contain at least 1 special character`;
-
-export const minLength: number = 8;
-export const minLengthErrorMessage: string = $localize`:@@password-strength-validator.Password-must-be-at-least-8-characters-long:Password must be at least 8 characters long`;
+export const minLength: number = 12;
+export const minLengthErrorMessage: string = $localize`:@@password-strength-validator.Password-must-be-at-least-12-characters-long:Password must be at least 12 characters long`;
 
 export const validatePassword = function (password: string): string[] {
   const validationResult: [boolean, string][] = [
-    [lowerCaseRegex.test(password), lowerCaseNoMatchErrorMessage],
-    [upperCaseRegex.test(password), upperCaseNoMatchErrorMessage],
-    [digitRegex.test(password), digitNoMatchErrorMessage],
-    [specialCharRegex.test(password), specialCharNoMatchErrorMessage],
     [password.length >= minLength, minLengthErrorMessage],
   ]
 
