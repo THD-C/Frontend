f382aa2f72003befd215ed156ebbb96614aa2ba3
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-12-14 11:55:23 +0100

    fix env prod api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://Frontend_API:8000/api',
+  apiUrl: 'http://thdc:8000/api',
   google: {
     clientId: '664863157184-050m2q9hh8msvp9ik6vmrg0l0lkrpqo4.apps.googleusercontent.com',
     scopes: [
