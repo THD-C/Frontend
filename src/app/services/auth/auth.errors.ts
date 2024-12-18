7b3997a0e38bfd7bd5ba6b58a8205a118bded060
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-12-18 18:15:17 +0100

    add missing code error

--- src/app/services/auth/auth.errors.ts
+++ src/app/services/auth/auth.errors.ts
@@ -3,4 +3,5 @@ import { ApiError } from '../../shared/models/error.model';
 export const errors: ApiError = {
   'email_or_username_occupied': $localize`:@@auth-errors.E-mail-or-username-already-occupied:E-mail or username already occupied`,
   'invalid_credentials': $localize`:@@auth-erros.Credentials-are-invalid-Please-try-again-or-create-account:Credentials are invalid. Please try again or create account`,
+  'invalid_token': $localize`:@@auth-errors.Could-not-authenticate-with-external-provider-Please-try-again-or-contact-support:Could not authenticate with external provider. Please try again or contact support`,
 }
