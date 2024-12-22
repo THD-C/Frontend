24320deb3dca757018e86dc2a43fcb75b2b7a00c
GringoXY <przemyslawkowalski01pk@gmail.com>
2024-12-22 18:42:09 +0100

    fix not passing password like fields to the API request

--- src/app/interceptors/trace-id/trace-id.interceptor.ts
+++ src/app/interceptors/trace-id/trace-id.interceptor.ts
@@ -29,7 +29,9 @@ function handleRequest(req: HttpRequest<unknown>, next: HttpHandlerFn): Observab
 
   const tracer = trace.getTracer(serviceName);
   const span = tracer.startSpan(`${req.method} ${req.url}`);
-  let clearedReqBody = req.body;
+
+  // Removing reference
+  let clearedReqBody = JSON.parse(JSON.stringify(req.body));
 
   // Clearing body from sensitive data
   if (clearedReqBody?.hasOwnProperty('password')) {
