2685a71fbc68f721fb27388c10885e22ab214dcc
Smithski <62666278+GringoXY@users.noreply.github.com>
2024-12-22 18:52:53 +0100

    Add theme switch (#59)
    * add opentelemetry
    
    * add service name to opentelemetry
    
    * add trace-id interceptor
    
    * fix otel tracing
    
    * add dark theme
    
    * fix not passing password like fields to the API request
    
    * add translations
    
    * remove otel user interactions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { SimpleNotificationsModule } from 'angular2-notifications';
 import { appName } from './app.config';
-import { DonateComponent } from "./shared/components/donate/donate.component";
+import { DonateComponent } from './shared/components/donate/donate.component';
+import { ThemesService } from './services/themes/themes.service';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,14 @@ import { DonateComponent } from "./shared/components/donate/donate.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   readonly title = appName;
+
+  constructor(private readonly themesService: ThemesService) {}
+
+  ngOnInit(): void {
+    this.themesService.loadCached();
+  }
 
 }
